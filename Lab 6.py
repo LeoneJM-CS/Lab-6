82fd73b12fb2b9cfb0a487032925e50ffc2dec2c
LeoneJM-CS <LeoneJM@jbu.edu>
2024-12-08 13:27:17 -0600

    Add files via upload

--- Lab 6.py
+++ Lab 6.py
@@ -3,20 +3,22 @@ import pickle
 
 courseList = []
 while 1:
+    print(" ")
     print("1. Add Course and Students")
-    print("2. Display Courses and Student's")
+    print("2. Display Courses and Students")
     print("0. Exit Program")
     choice = int(input())
+    print(" ")
 
     if choice == 1:
         cc = ls.Course_grades()
         cc.course()
         courseList.append(cc)
-        d = open('Courses.dat', 'ab')
+        d = open('Courses_Project.dat', 'ab')
         pickle.dump(cc, d)
         d.close()
     if choice == 2:
-        d = open('Courses.dat', 'rb')
+        d = open('Courses_Project.dat', 'rb')
         while 2:
             try:
                 data = pickle.load(d)
